7d9f297d69dbfe68e48b99269f2c1b7a7ead1c34
Chewie <efcnzm1@gmail.com>
2025-07-09 16:01:51 +0300

    Router yapılandırması

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/demasteknoloji">
       <App />
     </BrowserRouter>
   </React.StrictMode>
