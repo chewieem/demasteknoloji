553dabbad35cfe334f5459e710518310fd0b8e2a
Chewie <efcnzm1@gmail.com>
2025-07-09 17:44:44 +0300

    Baseurl TR

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/demasteknoloji">
+    <BrowserRouter basename="/demasteknoloji/tr">
       <App />
     </BrowserRouter>
   </React.StrictMode>
